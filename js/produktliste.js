6040c7068b056ee85b957dbc3aa8274aa93c48e5
Jonathan <joda0003@stud.kea.dk>
2024-09-17 12:33:44 +0200

    Ændringer af JS

--- js/produktliste.js
+++ js/produktliste.js
@@ -1,8 +1,9 @@
 const urlParams = new URLSearchParams(window.location.search);
 const category = urlParams.get("category");
+const encoded = encodeURIComponent(category);
 console.log(category);
 
-fetch(`https://eitpzfzwqrjncnwgbuqg.supabase.co/rest/v1/techandstory?category=eq.${category}`, {
+fetch(`https://eitpzfzwqrjncnwgbuqg.supabase.co/rest/v1/techandstory?category=eq.${encoded}`, {
   headers: {
     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpdHB6Znp3cXJqbmNud2didXFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NjM0MDAsImV4cCI6MjA0MTUzOTQwMH0.8FkKbLapnt5g8peUsqds68gBza1mQ4Kbp0h5IjMngZo",
   },
@@ -27,7 +28,6 @@ function vis(data) {
   data.forEach((item) => {
     const template = document.querySelector("#smallProductTemplate").content;
     const copy = template.cloneNode(true);
-
     copy.querySelector("h3").textContent = item.produktnavn;
     copy.querySelector(".subtle").textContent = item.type;
 
